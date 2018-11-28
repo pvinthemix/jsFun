c6a18973d19fbf55a651557de4ad709d115e64d0
Paul Vangelakos <paulvangelakos@gmail.com>
2018-11-28 08:37:17 -0700

    working on jsFUN

--- context/index.js
+++ context/index.js
@@ -16,11 +16,11 @@ const context = {
 
 
     // What is the value of `this` when we call ship.fly()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // The value of 'this' references the global window object in this context because the fly function is declared/created with
   },
 
   exerciseB() {
@@ -30,7 +30,7 @@ const context = {
     }
     
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
@@ -49,7 +49,7 @@ const context = {
     el.addEventListener('click', car.getInfo);
 
     // What is the value of `this` when a user clicks on our element and car.getInfo() is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
@@ -70,11 +70,11 @@ const context = {
 
 
     // What is the value of `this` when we call dog.getBreed()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // Inner function is not a method on dog, it is within getBreed so it loses it context
   },
 
   exerciseE() {
@@ -86,11 +86,11 @@ const context = {
 
 
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' references the global window object in this context and adheres to rule 1 as the default value because there is not object to reference besides the window.  It is not a method on another object so it is not rule number 2, and we are not using the new keyword so it does not point to a newly created object. 
   },
 
   exerciseF() {
@@ -109,7 +109,7 @@ const context = {
     const storm = new Hero('Ororo', 'weather control', true);
 
     // What is the value of `this` when we call storm.identifyHero()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'instance of Hero';
     return result;
 
     // Annotation: 
@@ -137,11 +137,11 @@ const context = {
 
 
     // What is the value of `this` when we call monopoly.restart()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' refers to the global window object using rule 1. Since 'this' lives within setTimeout() which is within restart(). Since setTimeout() is not a direct method on the object or a new instance of the object it defaults ot the global window object.
   },
 
   exerciseH() {
@@ -157,11 +157,13 @@ const context = {
     obj.method();
 
     // What is the value of `this` when we call obj.arrowFunction()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'obj';
     return result;
 
+
+
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' refers to the 'obj' object because we are using a ES6 arrow function to reassign the value of the arrowFunction property and binding the value to when it is declared. Additionally, this follows rule 2 because arrowFunction is being executed as a method on the object referring to the parent object 'obj'. 
   },
 
   exerciseI() {  
@@ -180,11 +182,12 @@ const context = {
     }, poets);
 
     // What is the value of `this` that gets returned on each iteration of poets.map()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'poets';
     return result;
 
-    // Annotation: 
-    // Write your annotation here as a comment. Annotation should include explanation regarding the second argument of `poets` that is being passed
+    // Annotation:  
+    // 'this' refers to the poets object since the poets object is being used as the second argument in map prototype method. This second argument is called 'thisArg' and is the value we are telling the prototype we want to use as 'this' when executing callback.
+
   },
 
   exerciseJ() {
@@ -194,11 +197,11 @@ const context = {
     });
 
     // What is the value of `this` when a user clicks on our #btn element and the callback is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment.
+    // 'this' refers to the object el because of rule 2. 'el' is an object on the DOM and which has access to the .on method. This makes the method refer to it's parent object which is the object el.
   },
 
   exerciseK() {
